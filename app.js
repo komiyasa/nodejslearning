f4379493b1ca4ce39ae0b83cb35a9945e56f7c11
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-10-08 20:49:20 +0900

    bootstrapへ変更し、ルーティングを追加

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const other_page = fs.readFileSync('./other.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer(getFromClient);
 
@@ -10,12 +13,34 @@ server.listen(3000);
 console.log('Server Start!');
 
 function getFromClient(request, response){
-    var content = ejs.render(index_page, {
-        title : "Index ページ",
-        content : "これはテンプレを使ってプログラムから値を参照しています"
-    });
-    response.writeHead(200, { 'Content-Type' : 'text/html' });
-    response.write(content);
-    response.end();
+    var url_parts = url.parse(request.url);
+    switch (url_parts.pathname){
+
+        case '/':
+            var content = ejs.render(index_page, {
+                title : "Index ページ",
+                content : "これはテンプレを使ってプログラムから値を参照しています"
+            });
+            response.writeHead(200, { 'Content-Type' : 'text/html' });
+            response.write(content);
+            response.end();
+            break;
+
+        case '/other':
+            var content = ejs.render(other_page, {
+                title : "Otherのページ",
+                content : "これは新しく用意したページです"
+            });
+            response.writeHead(200, { 'Content-Type' : 'text/html' });
+            response.write(content);
+            response.end();
+            break;
+
+        default:
+            response.writeHead(200, { 'Content-Type' : 'text/html' });
+            response.end('no page...');
+            break;
+
+    }
 }
     
