8444bd6196eee0b8a086805f9313a3b28a4529f1
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-10-08 23:18:36 +0900

    テーブル形式に表示される

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
@@ -12,41 +13,85 @@ var server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server Start!');
 
+//Create Server で処理する関数
 function getFromClient(request, response){
     var url_parts = url.parse(request.url, true);
     switch (url_parts.pathname){
 
         case '/':
-
-            var content = "これは Index のページです。"
-            var query = url_parts.query;
-            if(query.msg != undefined){
-                content += 'あたなは、「' + query.msg + '」と送りました。'; 
-            }
-            var content = ejs.render(index_page, {
-                title : "Index",
-                content : content,
-            });
-            response.writeHead(200, { 'Content-Type' : 'text/html' });
-            response.write(content);
-            response.end();
+            response_index(request, response);
             break;
 
         case '/other':
-            var content = ejs.render(other_page, {
-                title : "Otherのページ",
-                content : "これは新しく用意したページです"
-            });
-            response.writeHead(200, { 'Content-Type' : 'text/html' });
-            response.write(content);
-            response.end();
+            response_other(request, response);
             break;
 
+        case '/style.css':
+            response.writeHead(200, { 'Content-Type' : 'text/css' });
+            response.write(style_css);
+            response.end();
+            break;
+        
         default:
             response.writeHead(200, { 'Content-Type' : 'text/plain' });
             response.end('no page...');
             break;
-            
     }
 }
-    
+
+var data = {
+    'Taro' : '09-999-999',
+    'Hanako' : '080-888-888',
+    'Sachiko' : '070-777-777',
+    'Ichiro' : '060-666-666'
+};
+
+//Index のアクセス処理
+function response_index(request, response){
+    var msg = "これは Index のページです。"
+    var content = ejs.render(index_page, {
+        title : "Index",
+        content : msg,
+        data : data,
+    });
+    response.writeHead(200, { 'Content-Type' : 'text/html' });
+    response.write(content);
+    response.end();
+}
+
+//Oteher のアクセス処理
+function response_other(request, response){
+    var msg = "これは Other のページです。"
+    if(request.method == 'POST'){
+        var body = '';
+
+        //データ受信のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        //データ受信終了時の処理
+        request.on('end', ()=> {
+            var post_data = qs.parse(body);
+            msg += 'あなたは、「' + post_data.msg + '」と書きました';
+            var content = ejs.render(other_page, {
+                title : "Other",
+                content : msg,
+            });
+            response.writeHead(200, { 'Content-Type' : 'text/html' });
+            response.write(content);
+            response.end();
+        })
+    } else {
+        var msg = "ページがありません"
+        var content = ejs.render(other_page, {
+            title : "Other",
+            content : msg,
+        });
+        response.writeHead(200, { 'Content-Type' : 'text/html' });
+        response.write(content);
+        response.end();
+    }
+}
+
+              
