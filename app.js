2a655c110282bba134caf78a03935a2476be0c6f
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-10-09 17:57:25 +0900

    Cookie を扱ってみるところまで

--- app.js
+++ app.js
@@ -39,12 +39,7 @@ function getFromClient(request, response){
     }
 }
 
-var data = {
-    'Taro' : '09-999-999',
-    'Hanako' : '080-888-888',
-    'Sachiko' : '070-777-777',
-    'Ichiro' : '060-666-666'
-};
+var data = { msg : 'no message...'};
 
 var data2 = {
     'Ichiro' : ['最強のアベレージヒッター', '09-999-999', 'Meiden'],
@@ -55,16 +50,57 @@ var data2 = {
 
 //Index のアクセス処理
 function response_index(request, response){
-    var msg = "これは Index のページです。"
+    if(request.method=='POST'){
+        var body = '';
+
+        //データ受信のイベント処理
+        request.on('data',(data) => {
+            body += data;
+        });
+
+        //データ受信のイベント処理
+        request.on('end', ()=>{
+            data = qs.parse(body);
+            //クッキーの保存
+            setCookie('msg', data.msg, response);
+            write_index(request, response);
+        });
+    } else {
+        write_index(request,response);
+    }
+}
+
+function write_index(request, response){
+    var msg = "伝言を表示します"
+    var cookie_data = getCookie('msg', request);
     var content = ejs.render(index_page, {
         title : "Index",
         content : msg,
         data : data,
-        filename : 'data_item'
+        cookie_data : cookie_data,
     });
     response.writeHead(200, { 'Content-Type' : 'text/html' });
     response.write(content);
     response.end();
+}
+
+//クッキーの値を設定
+function setCookie(key, value, response){
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+// クッキーの値を取得
+function getCookie(key, request){
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data){
+        if(data[i].trim().startsWith(key + '=')){
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);
+        }
+    }
+    return '';
 }
 
 //Oteher のアクセス処理
